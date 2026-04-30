8f39c91e08a91fa6fbaf3e29082dbfd691f9f81d
LeskarX <leskarzone123@gmail.com>
2026-04-30 13:16:24 +0530

    schema and DataBase setup

--- src/server.js
+++ src/server.js
@@ -1,1 +1,25 @@
-hjh
+import app from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.config.js";
+
+dotenv.config();
+
+
+const PORT = process.env.PORT || 3000;
+const startServer = async () => {
+    try {
+      // connect database
+      await connectDB();
+  
+      // start server ONLY after DB connects
+      app.listen(PORT, () => {
+        console.log(` Server running on port ${PORT}`);
+      });
+  
+    } catch (error) {
+      console.error('Failed to start server:', error);
+      process.exit(1);
+    }
+  };
+  
+  startServer();
