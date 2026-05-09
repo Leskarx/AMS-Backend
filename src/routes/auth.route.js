cacd0ead9d8c250c36143f16d6b037f089764d91
LeskarX <leskarzone123@gmail.com>
2026-05-09 16:18:42 +0530

    allowed only admin to create user

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import  loginController  from '../controllers/login.controller.js';
 import registerController from '../controllers/register.controller.js';
+import authMiddleware from "../middlewares/auth.middleware.js";
+import roleMiddleware from "../middlewares/role.middleware.js";
 
 const router = Router();
 
 router.post('/login',loginController);
-router.post('/register',registerController);
+router.post('/register',
+    authMiddleware,
+    roleMiddleware(["ADMIN"]), // Only ADMIN can register new users
+    registerController);
 
 export default router;
 
